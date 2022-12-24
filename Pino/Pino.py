c5ea961beb4ab6ec9e6a79bb3ae100dfc659fd00
LumiNovryM <luminovrymekel@gmail.com>
2022-12-24 22:55:43 +0700

    Pino-Bot : Commands & Query

--- Pino/Pino.py
+++ Pino/Pino.py
@@ -57,5 +57,69 @@ def list_kelas(message):
     else :
         Pinobot.reply_to(message, "Anda Belum Terdaftar Sebagai Admin\nHubungi @lumi_novry")
 
+# Absensi 
+@Pinobot.message_handler(commands=['check-absensi'])
+def check_absensi(message):
+    user_id = message.chat.id
+    sql.execute("SELECT chat_id FROM bot_admins WHERE chat_id = {}".format(user_id))
+    result = sql.fetchone()
+    print(result)
+    if(result):
+        texts = message.text.split(' ')
+        kelas = str(texts[1])
+        kelas = kelas.replace('-',' ')
+        mapel = str(texts[2])
+        mapel = mapel.replace('-',' ')
+        tanggal = str(texts[3])
+        sql.execute("SELECT absen_siswas.tanggal, absen_siswas.keterangan, gurus.name, siswas.nama_siswa, kelas.kelas, mapels.pelajaran FROM absen_siswas INNER JOIN gurus ON absen_siswas.guru_id=gurus.id INNER JOIN siswas ON absen_siswas.siswa_id=siswas.id INNER JOIN kelas ON absen_siswas.kelas_id=kelas.id INNER JOIN mapels ON absen_siswas.mapel_id=mapels.id WHERE tanggal LIKE '%{}%' AND kelas.kelas LIKE '%{}%' AND mapels.pelajaran LIKE '%{}%'".format(tanggal, kelas, mapel))
+        result = sql.fetchall()
+        if(result):
+            detail = result[0]
+            tanggal_absen = detail[0]
+            nama_guru = detail[2]
+            mapel = detail[5]
+            final = ''        
+            for data in range (len(result)):
+                final = final + str(data+1) + '.)' + str(result[data][3]) + ' : ' + str(result[data][1]) + '\n'
+            print(final)
+            pesan_balasan = "Detail Absensi :\nTanggal : {}\nGuru : {}\nMata Pelajaran : {}\n==================\nDaftar Absensi Siswa:\n{}".format(tanggal_absen, nama_guru, mapel, final)
+            Pinobot.reply_to(message, pesan_balasan)
+        else: 
+            Pinobot.reply_to(message, "Data Tidak Ditemukan")
+    else :
+        Pinobot.reply_to(message, "Anda Belum Terdaftar Sebagai Admin\nHubungi @lumi_novry")
+
+# Absensi 
+@Pinobot.message_handler(commands=['check-absensi-alpha'])
+def check_absensi(message):
+    user_id = message.chat.id
+    sql.execute("SELECT chat_id FROM bot_admins WHERE chat_id = {}".format(user_id))
+    result = sql.fetchone()
+    print(result)
+    if(result):
+        texts = message.text.split(' ')
+        kelas = str(texts[1])
+        kelas = kelas.replace('-',' ')
+        mapel = str(texts[2])
+        mapel = mapel.replace('-',' ')
+        tanggal = str(texts[3])
+        sql.execute("SELECT absen_siswas.tanggal, absen_siswas.keterangan, gurus.name, siswas.nama_siswa, kelas.kelas, mapels.pelajaran FROM absen_siswas INNER JOIN gurus ON absen_siswas.guru_id=gurus.id INNER JOIN siswas ON absen_siswas.siswa_id=siswas.id INNER JOIN kelas ON absen_siswas.kelas_id=kelas.id INNER JOIN mapels ON absen_siswas.mapel_id=mapels.id WHERE tanggal LIKE '%{}%' AND kelas.kelas LIKE '%{}%' AND mapels.pelajaran LIKE '%{}%' AND absen_siswas.keterangan NOT IN ('Hadir')".format(tanggal, kelas, mapel))
+        result = sql.fetchall()
+        if(result):
+            detail = result[0]
+            tanggal_absen = detail[0]
+            nama_guru = detail[2]
+            mapel = detail[5]
+            final = ''        
+            for data in range (len(result)):
+                final = final + str(data+1) + '.)' + str(result[data][3]) + ' : ' + str(result[data][1]) + '\n'
+            print(final)
+            pesan_balasan = "Detail Absensi :\nTanggal : {}\nGuru : {}\nMata Pelajaran : {}\n==================\nDaftar Absensi Siswa:\n{}".format(tanggal_absen, nama_guru, mapel, final)
+            Pinobot.reply_to(message, pesan_balasan)
+        else: 
+            Pinobot.reply_to(message, "Data Tidak Ditemukan")
+    else :
+        Pinobot.reply_to(message, "Anda Belum Terdaftar Sebagai Admin\nHubungi @lumi_novry")
+
 # Keep Update
 Pinobot.polling(none_stop=True)
